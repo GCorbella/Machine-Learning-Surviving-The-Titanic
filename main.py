d54233f0233e2946b5951f978d96c71b3b5e1e89
GCorbella <guillepcorbella@hotmail.com>
2022-07-05 14:02:45 +0200

    Script and requiremtns created. Libraries imported

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix
+from sklearn import tree
+
